b15d30e2354391b9b69389e0753f3433cb47d595
n3lsc <nellalyse2@yandex.ru>
2025-10-01 19:17:43 +0700

    feat: initial Django project setup

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def home_view(request):
+    return HttpResponse("<h1>Добро пожаловать в мой блог!</h1>")
