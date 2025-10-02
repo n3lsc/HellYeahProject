96273a1fb64239822b3e4a657546cab23bcfebd2
n3lsc <nellalyse2@yandex.ru>
2025-10-02 17:08:04 +0700

    feat: updated post model with some tests

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import Post
+from django.utils import timezone
+
+class PostModelTest(TestCase):
+
+    def setUp(self):
+        self.post = Post.objects.create(
+            title ="Test Title",
+            content ="Test Content"
+        )
+    
+    def test_post_creation(self):
+        self.assertEqual(self.post.title, "Test Title")
+        self.assertEqual(self.post.content, "Test Content")
+        self.assertTrue(self.post.created_at <= timezone.now())
+
+    def test_post_str(self):
+        self.assertEqual(str(self.post), "Test Title")
+#        self.assertEqual(str(self.post), "Test Content")
 
 # Create your tests here.
