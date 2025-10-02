ec72cafc8680e03fab8acadd7d6f9682bab0ab13
n3lsc <nellalyse2@yandex.ru>
2025-10-02 16:27:44 +0700

    feat: added about page

--- posts/views.py
+++ posts/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 
 def home_view(request):
     return HttpResponse("<h1>Добро пожаловать в мой блог!</h1>")
+def about_view(request):
+    return HttpResponse("<h1>Hell yeah 100'%' about</h1>")
