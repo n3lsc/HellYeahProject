5a844afa00936342c3c0454a66f42cfce5b05340
n3lsc <nellalyse2@yandex.ru>
2025-10-03 08:30:03 +0700

    feat: added author for posts

--- posts/models.py
+++ posts/models.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    author = models.CharField(max_length=100)
     created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
